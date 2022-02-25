eb6e314893009b417b3d348be6f7ba99011fb3d8
Sean McGillis <slcidevice@yahoo.com>
2022-02-24 20:04:52 -0700

    Added better dummy product info

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,21 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+
+  const products = [
+    {title: 'Shoes', price: 22.50, description: 'It\'s like walking on a cloud!'},
+    {title: 'Sweater', price: 35.99, description: 'I\'m like a human burrito in this thing!'},
+    {title: 'Sunglasses', price: 13.00, description: 'I finally look as cool as I feel.'},
+  ]
+
+  const productList = products.map(product => (
+    <ProductItem product={product} key={Math.random()}/>
+  ))
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {productList}
       </ul>
     </section>
   );
